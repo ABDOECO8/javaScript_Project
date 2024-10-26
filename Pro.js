2db21dd60c3b4424086923a5f82d73fea886d9c9
Abdelkrim El-Fetouaki <kik.bad33@example.com>
2024-10-26 12:57:33 +0100

    map

--- Pro.js
+++ Pro.js
@@ -27,3 +27,34 @@ alert("thans for your infos");
 }
 
 
+const employees = [
+    { id: 1, name: 'John Doe', age: 30, department: 'IT', salary: 21000 },
+    { id: 2, name: 'Alice Smith', age: 28, department: 'HR', salary: 45000 },
+    { id: 3, name: 'Bob Johnson', age: 35, department: 'Finance', salary: 60000 },
+    //... More employee records can be added here
+  ];
+function displayEmployees() {
+  const totalEmployees = employees.map((item, index) => `<p>item ${index+1} = ${item.id}: ${item.name}: ${item.name} - ${item.department} - $${item.salary}</p>`).join('');
+  document.getElementById('employeesDetails').innerHTML = totalEmployees;
+}
+
+function calculateTotalSalaries(){
+    const T = employees.reduce((tot ,employee ) => tot + employee.salary , 0) ;
+    alert(`totale des salaire est : ${T}`)
+}
+
+function displayHREmployees() {
+    const HR = employees.find((e) => e.department === 'HR') ;
+  alert(`${HR.name}`)
+    const HRN = HR.map((t,i) => `<p>item ${i+1} : ${t.name}</p>`) ;
+    document.getElementById('employeesDetails').innerHTML = HRN;
+}
+function findEmployeeById(employeeId) {
+    const foundEmployee = employees.find(employee => employee.id === employeeId);
+    if (foundEmployee) {
+    document.getElementById('employeesDetails').innerHTML =`<p>${foundEmployee.id}: ${foundEmployee.name}: ${foundEmployee.name} - ${foundEmployee.department} - $${foundEmployee.salary}</p>`;
+    }
+    else{
+      document.getElementById('employeesDetails').innerHTML = 'no employee has been found with this ID';
+    }
+}
